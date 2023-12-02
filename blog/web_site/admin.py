0ad60e6a57496852ac28316e8c11c9719542164c
azamat-9933 <azamat.yakhyayeff@gmail.com>
2023-12-02 16:15:28 +0500

    a

---
+++ blog/web_site/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from .models import Category, Article, Favorite
+from modeltranslation.admin import TranslationAdmin
+
+
+
+
+class ArticleAdmin(TranslationAdmin):
+    list_display = ('pk', 'title', 'created_at', 'views', 'category', 'author')
+    list_display_links = ('pk', 'title')
+    list_filter = ('created_at', 'category', 'author')
+    list_editable = ('category', 'author')
+    readonly_fields = ('views',)
+
+
+class CategoryAdmin(TranslationAdmin):
+    list_display = ('pk', 'name')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Favorite)
+admin.site.register(Article, ArticleAdmin)
+
